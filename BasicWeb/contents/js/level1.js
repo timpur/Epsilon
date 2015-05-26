2b76ff2e998131bb22c6a99234d2905bc7ca33bb
timpur <timothy@purchas.com>
2015-05-26 11:32:00 +1000

    Web Update

--- BasicWeb/contents/js/level1.js
+++ BasicWeb/contents/js/level1.js
@@ -192,15 +192,19 @@ function gotToNextLevel() {
     var subLNumber = getSublevelNumber();
     if (subLNumber < Sublevels.length - 1) {
         sound1.play();
+        $("#theModal").find("#close").show();
+        $("#theModal").find("#close").text("Next Level");
         $("#theModal").modal('show');
         $("#theModal").on('hidden.bs.modal', function () {
             window.location = "level1.html?sublevel=" + Sublevels[subLNumber + 1];
         });
     } else {
+        sound1.play();
         $("#modalContent").html("You Have finished level 1");
+        $("#theModal").find("#close").hide();
         $("#theModal").modal('show');
         $("#theModal").on('hidden.bs.modal', function () {
-          window.location = "/results.html";
+          window.location = "/index.html";
         });
     }
 }
